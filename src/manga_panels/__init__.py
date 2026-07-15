f382003d2b08ff4e64b046e0a94bd71e38e97026
gfrcr <gabfranceira@hotmail.com>
2026-07-15 20:00:07 -0300

    chore: scaffold manga-panels package

---
+++ src/manga_panels/__init__.py
@@ -0,0 +1,7 @@
+from manga_panels.detect import Box, Detector, XYCutDetector, MLDetector, get_detector
+from manga_panels.pipeline import process_archive
+
+__all__ = [
+    "Box", "Detector", "XYCutDetector", "MLDetector", "get_detector",
+    "process_archive",
+]
